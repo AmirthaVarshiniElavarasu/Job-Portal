e7203849d0ed4f3562c6b7a4255046b210d54bb0
AmirthaVarshiniElavarasu <amirthavarhini@gmail.com>
2025-10-19 21:03:46 +0530

    deployment error cleared

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,31 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule,ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { JobPortalModule } from './job_portal/job_portal.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot( { isGlobal: true} ),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
-      imports:[ConfigModule],
-      inject:[ConfigService],
-      useFactory: async (configService: ConfigService)=> ({
-        url: configService.get('DATABASE_URL'),
-        ssl: { rejectUnauthorized: false }, // ✅ Required for Render DB
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres', // Explicitly define the driver
+        url: configService.get<string>('DATABASE_URL'),
+        ssl: {
+          rejectUnauthorized: false, //Required for Render DB SSL
+        },
         autoLoadEntities: true,
-        synchronize: true, // keep true for your internship project
+        synchronize: true, // Keep true for your internship project
         entities: [join(process.cwd(), 'dist/**/*.entity.js')],
-      })
+      }),
     }),
     JobPortalModule,
-    
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-
